473b45ad15f790ad91fcd08c8d0b6ca3b66384cb
Priyank <priyank@example.com>
2026-05-29 16:08:01 +0530

    done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -20,7 +20,6 @@ const lenis = new Lenis({
   duration: 1.2,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   smoothWheel: true,
-  smoothTouch: false,
 });
 
 function raf(time: number) {
